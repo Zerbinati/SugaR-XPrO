a955cae6188af6d344ebf5e00aa46b93df08430b
Marco Zerbinati <zerbinati.marco@gmail.com>
2023-08-08 13:58:05 +0200

    Adjust futility pruning base in qsearch
    Current master used value from transposition table there if it existed,
    this patch uses minimum between this tt value and the static eval instead
    (this thus is closer to the main search function, which uses the static eval).
    
    Passed STC:
    https://tests.stockfishchess.org/tests/view/64cd57285b17f7c21c0d6a8c
    LLR: 2.94 (-2.94,2.94) <0.00,2.00>
    Total: 252544 W: 64671 L: 64039 D: 123834
    Ptnml(0-2): 839, 29207, 65575, 29785, 866
    
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/64cf6c915b17f7c21c0d9fcb
    LLR: 2.95 (-2.94,2.94) <0.50,2.50>
    Total: 60150 W: 15374 L: 15012 D: 29764
    Ptnml(0-2): 24, 6321, 17024, 6681, 25
    
    closes #4725
    
    Bench: 1573024
     master
     stockfish-dev-20230807-5c2111cc
    @Vizvezdenec
    @snicolet
    Vizvezdenec authored

--- src/misc.cpp
+++ src/misc.cpp
@@ -286,7 +286,9 @@ std::string compiler_info() {
   #if defined(USE_MMX)
     compiler += " MMX";
   #endif
-  #if defined(USE_NEON)
+  #if defined(USE_NEON_DOTPROD)
+    compiler += " NEON_DOTPROD";
+  #elif defined(USE_NEON)
     compiler += " NEON";
   #endif
 
@@ -345,7 +347,7 @@ void show_logo()
  /_`   _  _  /_/ 
  _//_//_//_|/ \_
       __/
-	  
+
 )" << endl;
 
 #if defined(_WIN32)
@@ -1170,25 +1172,29 @@ static void* aligned_large_pages_alloc_windows([[maybe_unused]] size_t allocSize
       return nullptr;
 
   // Dynamically link OpenProcessToken, LookupPrivilegeValue and AdjustTokenPrivileges
-  HMODULE k32 = GetModuleHandle("Advapi32.dll");
-  auto fun6 = (fun6_t)(void(*)())GetProcAddress(k32, "OpenProcessToken");
+
+  HMODULE hAdvapi32 = GetModuleHandle(TEXT("advapi32.dll"));
+
+  if (!hAdvapi32)
+      hAdvapi32 = LoadLibrary(TEXT("advapi32.dll"));
+
+  auto fun6 = (fun6_t)(void(*)())GetProcAddress(hAdvapi32, "OpenProcessToken");
   if (!fun6)
       return nullptr;
-  auto fun7 = (fun7_t)(void(*)())GetProcAddress(k32, "LookupPrivilegeValueA");
+  auto fun7 = (fun7_t)(void(*)())GetProcAddress(hAdvapi32, "LookupPrivilegeValueA");
   if (!fun7)
       return nullptr;
-  auto fun8 = (fun8_t)(void(*)())GetProcAddress(k32, "AdjustTokenPrivileges");
+  auto fun8 = (fun8_t)(void(*)())GetProcAddress(hAdvapi32, "AdjustTokenPrivileges");
   if (!fun8)
       return nullptr;
-            
 
   // We need SeLockMemoryPrivilege, so try to enable it for the process
-  // OpenProcessToken()
-  if (!fun6(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hProcessToken))
-      return nullptr;
+  if (!fun6( // OpenProcessToken()
+      GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hProcessToken))
+          return nullptr;
 
-  // LookupPrivilegeValueA()
-  if (fun7(nullptr, SE_LOCK_MEMORY_NAME, &luid))
+  if (fun7( // LookupPrivilegeValue(nullptr, SE_LOCK_MEMORY_NAME, &luid)
+      nullptr, "SeLockMemoryPrivilege", &luid))
   {
       TOKEN_PRIVILEGES tp { };
       TOKEN_PRIVILEGES prevTp { };
@@ -1200,8 +1206,7 @@ static void* aligned_large_pages_alloc_windows([[maybe_unused]] size_t allocSize
 
       // Try to enable SeLockMemoryPrivilege. Note that even if AdjustTokenPrivileges() succeeds,
       // we still need to query GetLastError() to ensure that the privileges were actually obtained.
-      // AdjustTokenPrivileges()
-      if (fun8(
+      if (fun8( // AdjustTokenPrivileges()
               hProcessToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), &prevTp, &prevTpLen) &&
           GetLastError() == ERROR_SUCCESS)
       {
@@ -1211,8 +1216,8 @@ static void* aligned_large_pages_alloc_windows([[maybe_unused]] size_t allocSize
               nullptr, allocSize, MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES, PAGE_READWRITE);
 
           // Privilege no longer needed, restore previous state
-	  // AdjustTokenPrivileges ()
-          fun8(hProcessToken, FALSE, &prevTp, 0, nullptr, nullptr);
+          fun8( // AdjustTokenPrivileges ()
+              hProcessToken, FALSE, &prevTp, 0, nullptr, nullptr);
       }
   }
 
@@ -1466,7 +1471,7 @@ void init([[maybe_unused]] int argc, char* argv[]) {
     // extract the binary directory path from argv0
     binaryDirectory = argv0;
     size_t pos = binaryDirectory.find_last_of("\\/");
-    if (pos == string::npos)
+    if (pos == std::string::npos)
         binaryDirectory = "." + pathSeparator;
     else
         binaryDirectory.resize(pos + 1);
